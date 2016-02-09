ed55e072c212e9168860f9baa68f95aaf76f1883
Edward Ashton Brooks <P14131940@GWHXX582LIN002W.lec-admin.dmu.ac.uk>
2016-02-09 14:21:00 +0000

    Addition Added

---
+++ maths.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	int c, a, b, t;
+	printf("Enter function choice:\n'1' for +\t'2' for -\n'3' for *\t'4' for (division)\n");
+	scanf("%i", &c);
+	switch(c)
+	{
+	case 1:
+		printf("Enter 1st value:");
+		scanf("%i", &a);
+		printf("Enter 2nd value:");
+		scanf("%i", &b);
+		t = a + b;
+		printf("Total is %i\n", t);
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	default:
+		printf("ERROR");
+		break;
+	}
+	return 0;
+}
